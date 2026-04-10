e46264a25307824bc21321ef0385efb0424da5da
unknown <jamesverhoef2010@gmail.com>
2026-04-09 22:18:23 -0400

    Fixed name

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod parser;
 mod interpreter;
 
 fn main() {
-    let mut parser = Parser::new(">a>1210[@!>1+]".to_string());
+    let mut parser = Parser::new(">e@!!".to_string());
     let instructions = parser.parse();
     println!("{:?}", instructions);
 
